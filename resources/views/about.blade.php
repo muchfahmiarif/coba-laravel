3b6a2a9016364d4c89045904d0e2fc833de874d3
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-05 21:18:01 +0700

    `2` create extend (feature blade)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Coba Laravel | About</title>
-</head>
-<body>
+@extends('layouts.main')
+@section('container')
   <h1>Hello About</h1>
   <h3>{{$name}}</h3>
   <p>{{$email}}</p>
   <img src="/img/{{$image}}" alt="{{$name}}" width="200">
-</body>
-</html>
+@endsection
