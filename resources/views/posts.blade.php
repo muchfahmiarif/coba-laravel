d9e1012e4e77790f4afa93cb8e0cc90812cc40e8
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-05 21:40:04 +0700

    `2` add blog with a href to single post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,13 @@
-@dd($posts)
 @extends('layouts.main')
+
 @section('container')
-  <h1>Hello Posts</h1>
+  @foreach ($posts as $post)
+    <article class="mb-5">
+      <h2>
+        <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+      </h2>
+      <p>{{ $post["author"] }}</p>
+      <p>{{ $post["body"] }}</p>
+    </article>
+  @endforeach
 @endsection
