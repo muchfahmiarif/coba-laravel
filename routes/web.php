922c9a194771238424bc3bcf81b3191247dbb082
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-06 03:27:31 +0700

    `3` create new controller for all posts and single posts, change routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,16 +30,6 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/posts', function () {
-    return view('posts', [
-        'title' => 'Blog',
-        'posts' => Post::all()
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('posts/{slug}', function($slug) {
-    return view('post', [
-        'title' => 'Single Post',
-        'post' => Post::find($slug)
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
