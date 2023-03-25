4f78bf633020ee97021949c35317790e2f79e52b
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-03-25 12:26:48 +0700

    `12` redisgn ui, post latest will be on top

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,41 @@
 @section('container')
   <h1 class="mb-5">{{ $title }}</h1>
 
-  @foreach ($posts as $post)
+  @if ($posts->count())
+  <div class="card mb-3">
+    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
+    <div class="card-body text-center">
+      <h5 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h5>
+      <small>By <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{$posts[0]->category->slug}}" class="text-decoration-none">{{ $posts[0]->category->name }}</a></small>
+      <p class="card-text">{{ $posts[0]->excerpt }}</p>
+      <small><a href="/posts/{{$posts[0]->slug}}" class="text-decoration-none">Read More...</a></small>
+      <p class="card-text"><small class="text-body-secondary">{{ $posts[0]->created_at->diffForHumans() }}</small></p> {{-- diffForHumans adalah library carbon untuk mengatur waktu dan sudah ada di laravel --}}
+    </div>
+  </div>
+  @else
+    <p class="text-center fs-4">No Post Found.</p>
+  @endif
+
+  <div class="container">
+    <div class="row">
+      @foreach ($posts->skip(1) as $post)
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{ $post->title }}</h5>
+            <small>By <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{$post->category->slug}}" class="text-decoration-none">{{ $post->category->name }}</a></small>
+            <p class="card-text">{{ $post->excerpt }}</p>
+            <a href="/posts/{{$post->slug}}" class="text-decoration-none">Read More...</a>
+            <p class="card-text"><small class="text-body-secondary">{{ $post->created_at->diffForHumans() }}</small></p>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </div>
+
+  {{-- @foreach ($posts->skip(1) as $post)
     <article class="mb-5">
       <h2>
         <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
@@ -12,6 +46,6 @@
       <p>{{ $post->excerpt }}</p>
       <a href="/posts/{{$post->slug}}" class="text-decoration-none">Read More...</a>
     </article>
-  @endforeach
+  @endforeach --}}
 
 @endsection
