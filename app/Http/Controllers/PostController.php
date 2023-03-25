9dc01a9b8462663cd013671fe5944610db4c16fa
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-25 23:07:11 +0700

    `13` remove route category and author, add title with category and author

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,15 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
         // dd(request('search'));
+        $title = "";
+
+        if (request('category')) {
+            $title = " in " . Category::firstWhere('slug', request('category'))->name;
+        } elseif (request('author')) {
+            $title = " by " . User::firstWhere('username', request('author'))->name;
+        }
 
         return view('posts', [
-            'title' => 'All Post',
+            'title' => 'All Post' . $title,
             'active' => 'posts',
             'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get() // mengurutkan post dari yang terbaru dan melakukan eager loading
         ]);
--- routes/web.php
+++ routes/web.php
@@ -46,20 +46,3 @@ Route::get('/categories', function () {
         'categories' => Category::all(),
     ]);
 });
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view ('posts', [
-        'title' => "Post by Category : $category->name",
-        'active' => 'posts',
-        'posts' => $category->posts->load('category', 'author'),
-        // 'category' => $category,
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'active' => 'posts',
-        'posts' => $author->posts->load('category', 'author'),
-    ]);
-});
