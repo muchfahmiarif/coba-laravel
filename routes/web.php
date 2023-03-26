f03dbbf1ab3d06033bfa84a9b1aa869fe241e6bb
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-26 08:27:56 +0700

    `14` add view login page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,3 +45,5 @@ Route::get('/categories', function () {
         'categories' => Category::all(),
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
