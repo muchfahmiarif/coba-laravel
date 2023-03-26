77373aa8931240e7e4dd0311ff06709e0d974c2a
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-26 20:27:58 +0700

    `18` add new create post dashboard, add wildcard active url

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -6,6 +6,9 @@
 </div>
 
 <div class="table-responsive col-lg-8">
+
+  <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
+  
   <table class="table table-striped table-sm">
     <thead>
       <tr>
