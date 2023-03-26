3f480b33628e182196b91d0fb8a31e7fae998121
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-26 20:40:36 +0700

    `18` add new form for create new post

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -5,5 +5,18 @@
   <h1 class="h2">Create New Post</h1>
 </div>
 
-
+<div class="col-lg-8">
+  <form method="POST" action="/dashboard/posts"> {{-- Secara otomatis akan mengarah ke methode store pada resource dashboard controller --}}
+    @csrf
+    <div class="mb-3">
+      <label for="title" class="form-label">Title</label>
+      <input type="text" class="form-control" id="title" name="title">
+    </div>
+    <div class="mb-3">
+      <label for="slug" class="form-label">Slug</label>
+      <input type="text" class="form-control" id="slug" name="slug">
+    </div>
+    <button type="submit" class="btn btn-primary">Create Post</button>
+  </form>
+</div>
 @endsection
