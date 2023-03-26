10dbf22d273640749c42c4172616f8f6121982a9
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-26 11:31:15 +0700

    `16` add style navbar when login

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -18,9 +18,24 @@
             </li>
           </ul>
           <ul class="navbar-nav">
+            @auth
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Welcome back, {{ auth()->user()->name }}
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> Dashboard</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                  <a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Logout</a>
+                </li>
+              </ul>
+            </li>
+            @else
             <li class="nav-item">
               <a href="/login" class="nav-link {{($active === 'login' ? 'active' : '')}}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
             </li>
+            @endauth
           </ul>
         </div>
       </div>
